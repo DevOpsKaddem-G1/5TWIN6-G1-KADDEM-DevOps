91077adfe65197807264bd25d67c6d7424747281
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2023-10-17 18:34:36 +0100

    Remove this unused import

--- src/main/java/com/esprit/kaddem/Entities/Universite.java
+++ src/main/java/com/esprit/kaddem/Entities/Universite.java
@@ -1,6 +1,5 @@
 package com.esprit.kaddem.Entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +7,6 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.List;
 
 @Entity
 @Getter
