48c323612457d33139aa2899f4f31aff81ca5270
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2023-10-17 16:02:42 +0100

    Suppression de tous les fichiers de la branche

--- khaddem/src/main/java/tn/esprit/spring/khaddem/exceptions/UniversiteNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.spring.khaddem.exceptions;
-
-public class UniversiteNotFoundException extends RuntimeException {
-    public UniversiteNotFoundException(String message) {
-        super(message);
-    }
-}
