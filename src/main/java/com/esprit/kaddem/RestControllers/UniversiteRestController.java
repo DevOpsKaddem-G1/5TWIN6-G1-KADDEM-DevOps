9786b17acf80d4ee0181cebfa0590c54ae4f9d88
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2023-10-17 19:28:17 +0100

    Replace this persistent entity with a simple POJO or DTO object.
    vous séparez clairement les objets utilisés pour la communication API (DTO) de ceux utilisés pour la persistance des données (entités JPA). Cela améliore la clarté du code, la sécurité et le contrôle sur les données exposées via l'API

--- src/main/java/com/esprit/kaddem/RestControllers/UniversiteRestController.java
+++ src/main/java/com/esprit/kaddem/RestControllers/UniversiteRestController.java
@@ -1,6 +1,6 @@
 package com.esprit.kaddem.RestControllers;
 
-
+import com.esprit.kaddem.RestControllers.dtos.UniversiteDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.esprit.kaddem.Entities.Universite;
@@ -31,17 +31,28 @@ public class UniversiteRestController {
     // http://localhost:8089/Kaddem/universite/add-universite
     @PostMapping("/add-universite")
     @ResponseBody
-    public Universite addUniversite(@RequestBody Universite u) {
-        Universite universite = universiteService.addUniversite(u);
-        return universite;
+    public UniversiteDTO addUniversite(@RequestBody UniversiteDTO universiteDTO) {
+        Universite universite = new Universite();
+        universite.setNomUniv(universiteDTO.getNomUniv());
+
+        // Utilisez le service pour ajouter l'entité Universite
+
+        UniversiteDTO responseDTO = new UniversiteDTO();
+        responseDTO.setNomUniv(universite.getNomUniv());
+
+        return responseDTO;
     }
 
     // http://localhost:8089/Kaddem/universite/update-universite
     @PutMapping("/update-universite")
     @ResponseBody
-    public Universite updateUniversite(@RequestBody Universite u) {
-        Universite universite= universiteService.updateUniversite(u);
-        return universite;
+    public Universite updateUniversite(@RequestBody UniversiteDTO universiteDTO) {
+        Universite universite = new Universite();
+        universite.setNomUniv(universiteDTO.getNomUniv());
+
+        Universite updatedUniversite = universiteService.updateUniversite(universite);
+
+        return updatedUniversite;
     }
 
 //    // http://localhost:8089/Kaddem/universite/assignUniversiteToDepartement/1/1
