3401288f33ad1241c1e0ecd794548a2654533cef
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2023-11-04 20:36:19 +0100

    fix relation

--- src/main/java/com/esprit/kaddem/entities/Universite.java
+++ src/main/java/com/esprit/kaddem/entities/Universite.java
@@ -1,12 +1,14 @@
 package com.esprit.kaddem.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Getter
@@ -19,9 +21,9 @@ public class Universite implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idUniversite;
     private String nomUniv;
-    /*@OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL)
     @JsonIgnore
-    private List<Departement> departements;*/
+    private List<Departement> departements;
 
     public Universite(Integer idUniversite, String nomUniv) {
         this.idUniversite = idUniversite;
