ae67bc3d10d69846a39ff1f45612eb73382f9583
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-11-06 10:57:49 +0100

    feat: adding EtudiantService to 'Services' - Etudiant

---
+++ src/main/java/com/esprit/kaddem/services/IEtudiantService.java
@@ -0,0 +1,16 @@
+package com.esprit.kaddem.services;
+
+import com.esprit.kaddem.entities.Etudiant;
+import com.esprit.kaddem.entities.Niveau;
+import com.esprit.kaddem.entities.Specialite;
+
+import java.util.List;
+
+public interface IEtudiantService {
+    List<Etudiant> retrieveAllEtudiants();
+    Etudiant addEtudiant(Etudiant e);
+    Etudiant updateEtudiant(Etudiant e);
+    Etudiant retrieveEtudiant(Integer idEtudiant);
+    void removeEtudiant(Integer idEtudiant);
+
+}
