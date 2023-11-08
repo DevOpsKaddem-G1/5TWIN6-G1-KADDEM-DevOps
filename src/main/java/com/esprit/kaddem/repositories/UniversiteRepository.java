ecc40e83433538cffe14f73145507bb4258419f6
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-11-08 13:54:31 +0100

    fix: removing external files from other branches from 'boudhina-elyes-etudiant' branch - Removing files & renaming

--- src/main/java/com/esprit/kaddem/repositories/UniversiteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.esprit.kaddem.repositories;
-
-import com.esprit.kaddem.entities.Universite;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UniversiteRepository extends JpaRepository<Universite, Integer> {
-}
