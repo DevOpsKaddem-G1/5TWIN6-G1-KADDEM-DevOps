63a3518db9f3e6babe20d460c5856a8de75fc687
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-11-08 21:33:53 +0100

    refact: removing unused imports - Services

--- src/main/java/com/esprit/kaddem/services/IEtudiantService.java
+++ src/main/java/com/esprit/kaddem/services/IEtudiantService.java
@@ -1,10 +1,7 @@
 package com.esprit.kaddem.services;
 
 import com.esprit.kaddem.entities.Etudiant;
-import com.esprit.kaddem.entities.Niveau;
-import com.esprit.kaddem.entities.Specialite;
 import com.esprit.kaddem.restcontrollers.dtos.EtudiantDTO;
-
 import java.util.List;
 
 public interface IEtudiantService {
