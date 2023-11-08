0ff27ea9a3bddb5cdbe28b7e301ba660d4ef8cd5
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-11-08 22:44:25 +0100

    refact: refactoring 'EtudiantDTO' based on SonarLint and SonarQube suggestions - JUnit tests

--- src/main/java/com/esprit/kaddem/restcontrollers/dtos/EtudiantDTO.java
+++ src/main/java/com/esprit/kaddem/restcontrollers/dtos/EtudiantDTO.java
@@ -13,31 +13,15 @@ public class EtudiantDTO {
         return idEtudiant;
     }
 
-    public void setIdEtudiant(Integer idEtudiant) {
-        this.idEtudiant = idEtudiant;
-    }
-
     public String getPrenomE() {
         return prenomE;
-    }
-
-    public void setPrenomE(String prenomE) {
-        this.prenomE = prenomE;
     }
 
     public String getNomE() {
         return nomE;
     }
-
-    public void setNomE(String nomE) {
-        this.nomE = nomE;
-    }
-
+    
     public Option getOp() {
         return op;
-    }
-
-    public void setOp(Option op) {
-        this.op = op;
     }
 }
