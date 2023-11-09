f55790d31e32094ae804d16b2382f2f6219fa97a
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2023-11-09 01:52:02 +0100

    consommation du crud front angular

---
+++ src/main/java/com/esprit/kaddem/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.esprit.kaddem;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*"); // This allows all origins, but you should restrict this in production.
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("PUT");
+        corsConfig.addAllowedMethod("DELETE");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setMaxAge(3600L); // 1 hour
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsFilter(source);
+    }
+}
--- src/main/java/com/esprit/kaddem/restcontrollers/UniversiteRestController.java
+++ src/main/java/com/esprit/kaddem/restcontrollers/UniversiteRestController.java
@@ -3,6 +3,8 @@ package com.esprit.kaddem.restcontrollers;
 import com.esprit.kaddem.restcontrollers.dtos.UniversiteDTO;
 import com.esprit.kaddem.services.UniversiteServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.esprit.kaddem.entities.Universite;
 import java.util.List;
@@ -54,5 +56,14 @@ public class UniversiteRestController {
         return universiteService.updateUniversite(universite);
     }
 
+    @DeleteMapping("/delete-universite/{universite-id}")
+    public ResponseEntity<Void> deleteUniversite(@PathVariable("universite-id") Integer universiteId) {
+        try {
+            universiteService.deleteUniversite(universiteId);
+            return ResponseEntity.ok().build(); // 200 OK
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build(); // 500 Internal Server Error
+        }
+    }
 
 }
