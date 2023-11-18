b08f5001be669d4626fba5df8e9db2c851e354ef
MelkiAmal <amal.melki@esprit.tn>
2023-11-18 03:48:00 +0100

    equipe Mockito

--- src/main/java/com/esprit/kaddem/restcontrollers/EquipeRestController.java
+++ src/main/java/com/esprit/kaddem/restcontrollers/EquipeRestController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/equipe")
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://192.168.33.10")
 public class EquipeRestController {
     IEquipeService equipeService;
 
